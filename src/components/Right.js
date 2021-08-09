4e4b85d29de8cbf4bb6ef45e5380f83941571007
Abhiuday Gupta <77210185+cp-Coder@users.noreply.github.com>
2021-08-09 21:40:39 +0530

    Update Right.js

--- src/components/Right.js
+++ src/components/Right.js
@@ -121,7 +121,7 @@ function Right() {
 				</Recommendation>
 			</FollowCard>
 			<BannerCard>
-				<img src="https://static-exp1.licdn.com/scds/common/u/images/promo/ads/li_evergreen_jobs_ad_300x250_v1.jpg" alt="" />
+				<img src="/images/job-ad.jpg" alt="" />
 			</BannerCard>
 		</Container>
 	);
